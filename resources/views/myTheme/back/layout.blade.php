076938038df3ff3f7106091eee1b3343790ac731
vicpride <vicpride@gmail.com>
2019-01-14 22:48:39 -0500

    up 15/01/19

--- resources/views/myTheme/back/layout.blade.php
+++ resources/views/myTheme/back/layout.blade.php
@@ -5,14 +5,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Dashboard Template · Bootstrap</title>
+  <title>Идеи и Идеалы | Панель администратора </title>
 
   <!-- Bootstrap core CSS -->
-  {{--
-  <link href="dist/css/bootstrap.min.css" rel="stylesheet"> --}}
 
   <!-- Custom styles for this template -->
-  <link href="css/back.css" rel="stylesheet"> {{-- Fontawsom css --}}
+  <link href="css/back.css" rel="stylesheet"> 
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
     crossorigin="anonymous">
 </head>
@@ -20,9 +18,10 @@
 <body>
 
   <!-- NAVBAR -->
-  <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
-    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+  <nav class="navbar navbar-light bg-light fixed-top flex-md-nowrap p-1 shadow">
+    <a class="navbar-brand bg-light col-sm-3 col-md-2 mr-0" href="{{ route('home') }}">Идеи и Идеалы</a>
+<!--     <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
+    <h3 class="text-primary ml-4 mb-0 mr-auto">Университеты</h2>
     <ul class="navbar-nav px-3">
       <li class="nav-item text-nowrap">
         <a class="nav-link" href="#">Sign out</a>
@@ -36,81 +35,7 @@
     <div class="row">
 
       <!-- SIDEBAR -->
-      <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-        <div class="sidebar-sticky">
-          <ul class="nav flex-column">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">
-                <span data-feather="home"></span>
-                Dashboard <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <span data-feather="file"></span>
-                Orders
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <span data-feather="shopping-cart"></span>
-                Products
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <span data-feather="users"></span>
-                Customers
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <span data-feather="bar-chart-2"></span>
-                Reports
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <span data-feather="layers"></span>
-                Integrations
-              </a>
-            </li>
-          </ul>
-
-          <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-            <span>Saved reports</span>
-            <a class="d-flex align-items-center text-muted" href="#">
-              <span data-feather="plus-circle"></span>
-            </a>
-          </h6>
-          <ul class="nav flex-column mb-2">
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <span data-feather="file-text"></span>
-                Current month
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <span data-feather="file-text"></span>
-                Last quarter
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <span data-feather="file-text"></span>
-                Social engagement
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <span data-feather="file-text"></span>
-                Year-end sale
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+        @include(env('THEME_BACK').'.back.sidebar')
       <!-- end sidebar -->
 
       <!-- CONTENT -->
