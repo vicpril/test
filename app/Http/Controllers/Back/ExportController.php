ab679c9a8540e73ff0a6373d6507c689245dd859
vicpride <vicpride@gmail.com>
2019-05-27 04:17:17 -0400

    exxport articles back

--- app/Http/Controllers/Back/ExportController.php
+++ app/Http/Controllers/Back/ExportController.php
@@ -14,8 +14,8 @@ class ExportController extends Controller
 {
     protected $repository;
     protected $action;
-    protected $path;
-    protected $filename;
+//     protected $path;
+    
 
     public function __construct(ExportRepository $e_rep)
     {
@@ -23,72 +23,15 @@ class ExportController extends Controller
 
         $this->action = Route::current()->parameter('action');
 
-        $this->path = ".tmp/";
-        $this->filename = "export_{$this->action}_" . date('d-m-Y') . ".docx";
-
-        $this->prepare = "prepareTo_$this->action";
-
     }
 
     public function index(ExportRequest $request)
     {
-// dd($request->all());
-
-        $data = $this->repository->{$this->prepare}($request->get('articles'));
-
-        $this->createFile($data);
+        $this->repository->exportFile($this->action, $request->except('_token'))
+                         ->createFile()
+                         ->downloadFile();
     }
 
-    public function createFile ($data)
-    {
-        Blade::include(env('THEME_BACK') . ".back.export.{$this->action}._header", 'header');
-        Blade::include(env('THEME_BACK') . ".back.export.{$this->action}._body", 'body');
-        Blade::include(env('THEME_BACK') . ".back.export.{$this->action}._footer", 'footer');
-
-        $content = view(env('THEME_BACK') . ".back.export.{$this->action}.index", $data)->render();
-
-        $headers = [
-            'Content-Type' => 'application/doc',
-            'Content-Disposition' => "attachment; filename='{$this->filename}'"
-        ];
-
-        $phpWord = new \PhpOffice\PhpWord\PhpWord();
-
-
-        $section = $phpWord->addSection();
-
-
-        $description = "123123";
-
-
-        $section->addText($description);
-
-
-        $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
-        try {
-            $objWriter->save(public_path('helloWorld.docx'));
-        } catch (Exception $e) {
-        }
-
-        return response()->download(public_path('helloWorld.docx'))->send();
-        exit;
-
-
-            return response()->download($this->path . $this->filename)->send()->deleteFileAfterSend();
-            
-        return response()->streamDownload(function () use ($content) {
-            echo $content;
-        }, $this->filename)->send();
-
-        // File::put($this->path . $this->filename, $content);
-
-        // return $this;
-    }
-
-    public function downloadFile ()
-    {
-
-        return response()->download($this->path, $this->filename)->deleteFileAfterSend(true);
-    }
+    
 
 }
--- app/Repositories/ExportRepository.php
+++ app/Repositories/ExportRepository.php
@@ -8,16 +8,93 @@ use App\Models\Article;
 
 class ExportRepository extends Repository
 {
+    protected $action;
+  
+    protected $filename;
+    protected $temp_file_uri;
+    protected $prepare;
+  
+    protected $dataPrepeared;
+  
     public function __construct(Article $article)
     {
         $this->model = $article;
     }
-
+    
+    /*
+    *     Enter method
+    */
+    public function exportFile($action, $data) {
+        
+        $this->action = $action;
+      
+        $this->filename = "export_{$this->action}_" . date('d-m-Y') . ".docx";
+      
+        $this->temp_file_uri = tempnam('','temp_file');
+      
+        $this->prepare = "prepareTo_{$this->action}";
+      
+        return $this->{$this->prepare}($data);
+    }
+  
+  
+    /*
+    *   Prepare data to RINC
+    */
     public function prepareTo_rinc($articles) 
     {
-        $result = ['result' => date('D, d M Y H:i:s') . '<br>' . print_r($articles, true)];
+        $this->dataPrepeared = ['result' => date('D, d M Y H:i:s') . '<br>' . print_r($articles, true)];
 
-        return $result;
+        return $this;
+    }
+  
+  
+  
+  
+    
+    public function createFile ()
+    {
+        if (
+          !$this->dataPrepeared ||
+          !$this->temp_file_uri
+        ) {
+            throw new Exception('Data for export is not prepared', 500);
+        }
+      
+        // Create content
+        $content = view(env('THEME_BACK') . ".back.export.{$this->action}", $this->dataPrepeared)->render();
+
+        // Create temp file & past content
+        $phpWord = new \PhpOffice\PhpWord\PhpWord();
+        $section = $phpWord->addSection();
+        $section->addText($content);
+
+        // Save file
+        // Saving the document as OOXML file...
+        $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
+        $objWriter->save($this->temp_file_uri);
+
+        return $this;
+    }
+
+    public function downloadFile ()
+    {
+        if (
+          !$this->filename ||
+          !$this->temp_file_uri
+        ) {
+            throw new Exception('File for export is not prepared', 500);
+        }
+        //download code
+        header('Content-Description: File Transfer');
+        header("Content-Type: application/docx");//header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename='.$this->filename);
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Content-Length: ' . filesize($this->temp_file_uri));
+        readfile($this->temp_file_uri);
+        unlink($this->temp_file_uri); // deletes the temporary file
+        exit;
     }
 
 }
