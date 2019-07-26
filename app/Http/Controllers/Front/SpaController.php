351581eb7fee6b104420cbd6b86dc9ac1143f7fa
vicpride <vicpride@gmail.com>
2019-07-25 22:19:03 -0400

    corcel

--- app/Http/Controllers/Front/SpaController.php
+++ app/Http/Controllers/Front/SpaController.php
@@ -32,7 +32,57 @@ class SpaController extends SiteController
 
       dump($post);
       
-      // TAXONOMIES
+      /*
+      *  TAXONOMIES
+      *  Categories
+      */ 
+//       $tax_tr = Taxonomy::where('taxonomy', 'term_translations')
+//                       // ->where('term_id', 911)
+//                       // ->take(50)
+//                       ->get();
+//       $tax_tr = $tax_tr->map(function($item){
+//           return (unserialize($item->description)) ?: false;
+//       });
+
+//       // $tax_ru = Taxonomy::where('taxonomy', 'category')
+//       //                 ->where('term_id', 22)
+//       //                 ->take(15)
+//       //                 ->get()->first();
+      
+//       $tax_tr = $tax_tr->map(function($cat){
+//           $cat = collect($cat);
+//           $cat = $cat->map(function($tr_id){
+//               $tax = Taxonomy::where('taxonomy', 'category')
+//                           ->where('term_id', $tr_id)
+//                           ->first();
+
+//               return (is_object($tax))? $tax->name : false;
+//           });
+//           return $cat;
+//       })->reject(function($cat){
+//           return (isset($cat['ru']) && $cat['ru'] == false) || (isset($cat['en']) && $cat['en'] == false);
+//       });
+
+
+//       $tax_tr = $tax_tr->map(function($cat){
+//           return count($cat);
+//       });
+                      
+
+//       // dump($tax_ru);
+      
+
+//       var_dump($tax_tr->sum());                  
+
+      // $tag = $post->taxonomies()->where('taxonomy', 'post_tag')->first()->name;
+      // $cat = $post->taxonomies()->where('taxonomy', 'category')->first()->name;
+      
+      
+      /*
+      *  TAXONOMIES
+      *  TAGS
+      */ 
+      
       $tax_tr = Taxonomy::where('taxonomy', 'term_translations')
                       // ->where('term_id', 911)
                       // ->take(50)
@@ -46,33 +96,32 @@ class SpaController extends SiteController
       //                 ->take(15)
       //                 ->get()->first();
       
-      $tax_tr = $tax_tr->map(function($cat){
-          $cat = collect($cat);
-          $cat = $cat->map(function($tr_id){
-              $tax = Taxonomy::where('taxonomy', 'category')
+      $tax_tr = $tax_tr->map(function($tag){
+          $tag = collect($tag);
+          $tag = $tag->map(function($tr_id){
+              $tax = Taxonomy::where('taxonomy', 'post_tag')
                           ->where('term_id', $tr_id)
                           ->first();
 
               return (is_object($tax))? $tax->name : false;
           });
-          return $cat;
-      })->reject(function($cat){
-          return (isset($cat['ru']) && $cat['ru'] == false) || (isset($cat['en']) && $cat['en'] == false);
-      });
+          return $tag;
+      })
+        ->reject(function($tag){
+          return (isset($tag['ru']) && $tag['ru'] == false) || (isset($tag['en']) && $tag['en'] == false);
+      })
+        ;
 
 
-      $tax_tr = $tax_tr->map(function($cat){
-          return count($cat);
-      });
-                      
-
-      // dump($tax_ru);
+//       $tax_tr = $tax_tr->map(function($tag){
+//           return count($tag);
+//       });
+      
+//       // dump($tax_ru);
       
 
-      var_dump($tax_tr->sum());                  
-
-      // $tag = $post->taxonomies()->where('taxonomy', 'post_tag')->first()->name;
-      // $cat = $post->taxonomies()->where('taxonomy', 'category')->first()->name;
+      dump($tax_tr); 
+                      
       
       // dump($post->fileUpload);
 
