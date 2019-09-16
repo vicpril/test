bf60a5222450a76ba49c2f518bc93ad68601046f
vicpride <vicpride@gmail.com>
2019-09-16 05:08:13 -0400

    16/09/19

--- app/Http/Controllers/Front/ArticlesController.php
+++ app/Http/Controllers/Front/ArticlesController.php
@@ -222,7 +222,7 @@ class ArticlesController extends SiteController
 
         $issue = $this->i_rep->oneLastByStatus($this->status);
 
-        if ($issue) {
+        if ($issue->articles) {
             return redirect()->route('articles', [
                 'year' => $issue->year,
                 'no' => $issue->no,
